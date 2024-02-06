947c67dd3b991b13b248d4d6bc7ca0422718c7f9
Ingeborg Brommeland Austeid <ingeborgba@uia.no>
2024-02-06 14:36:07 +0100

    adds modification

--- src/Components/DragDropBox.tsx
+++ src/Components/DragDropBox.tsx
@@ -2,17 +2,34 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import Box from '@mui/system/Box';
 import { Button } from "@mui/material";
 import { FileUpload, Opacity } from "@mui/icons-material";
-import { green } from "@mui/material/colors";
 
-//Todo: Legge til droppable funksjonalitet
+async function verifyDrawings(formData: any) {
+    try {
+        const response = await fetch("http://localhost:8000", {
+            method: "POST",
+            body: formData,
+        });
+        const drawingResult = await response.json();
+        console.log(drawingResult)
+    } catch (error) {
+        console.error("Error:", error)
+    }
+    
+}
+
+//const files = document.querySelector('input[type="file"][multiple]');
+//const formData = new FormData();
+
+
 
 export default function DroppableBox() {
     return (
         <Droppable droppableId="droppable">
             {(provided) => (
             <Box component="section"sx={{ p: 2, border: '3px dashed green', borderRadius: '16px'}} ref={provided.innerRef} {...provided.droppableProps}>
-            Dra og slipp filene dine her &nbsp; &nbsp; <Button variant="contained"  endIcon={<FileUpload/>} color="success" sx={{ fontSize: 12}}>Last opp</Button> 
-        </Box>
+            Dra og slipp filene dine her &nbsp; &nbsp; <Button variant="contained"  endIcon={<FileUpload/>} color="success" sx={{ fontSize: 12}}>Last opp</Button>
+            {provided.placeholder} 
+            </Box>
         )}
         </Droppable>
         
